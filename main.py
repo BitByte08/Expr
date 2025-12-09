385d372d0ad9c54d0b19139bb51426bb570db5c1
bitbyte08 <me@bitworkspace.kr>
2025-12-09 10:52:25 +0900

    feat :: 라우터 등록, mcp mount

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi_mcp import FastApiMCP
+import uvicorn
+from routes.account import router as account_router
+from routes.league import router as league_router
+from routes.match import router as match_router
+from routes.rag import router as rag_router
+app = FastAPI()
+app.include_router(account_router, prefix="/account", tags=["account"])
+app.include_router(league_router, prefix="/league", tags=["league"])
+app.include_router(match_router, prefix="/match", tags=["match"])
+app.include_router(rag_router, prefix="/rag", tags=["rag"])
+
+mcp = FastApiMCP(app,
+                 name="Expr MCP",
+                 describe_all_responses=True,
+                 describe_full_response_schema=True
+                 )
+mcp.mount_http()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
